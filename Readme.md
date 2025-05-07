ff06c2382bde35cd34713e2c8554fd82252e6eb6
panachainy <panachai.ny@gmail.com>
2025-05-07 15:27:22 +0700

    back to one github token

--- Readme.md
+++ Readme.md
@@ -18,8 +18,7 @@ gh repo list $SOURCE_ORG --limit 1000 --json name,sshUrl -q ".[] | .sshUrl" | xa
 ```bash
 SOURCE_ORG=source_org_name
 DEST_ORG=destination_org_name
-GITHUB_TOKEN_SOURCE_ORG=your_github_token_source_org
-GITHUB_TOKEN_DEST_ORG=your_github_token_dest_org
+GITHUB_TOKEN=your_github_token_source_org
 
-./transfer-github.sh $SOURCE_ORG $DEST_ORG $GITHUB_TOKEN_SOURCE_ORG $GITHUB_TOKEN_DEST_ORG
+./transfer-github.sh $SOURCE_ORG $DEST_ORG $GITHUB_TOKEN
 ```
--- transfer-github.sh
+++ transfer-github.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Check if all required arguments are provided
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 <source_org> <dest_org> <source_token> <dest_token>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <source_org> <dest_org> <token>"
   exit 1
 fi
 
 SOURCE_ORG=$1
 DEST_ORG=$2
-GITHUB_TOKEN_SOURCE_ORG=$3
-GITHUB_TOKEN_DEST_ORG=$4
+GITHUB_TOKEN=$3
 
 # Initialize an empty array for repositories
 repos=()
@@ -18,7 +17,7 @@ page=1
 # Fetch repositories with pagination
 while true; do
   # Get repositories for current page
-  response=$(curl -s -H "Authorization: token $GITHUB_TOKEN_SOURCE_ORG" \
+  response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
     "https://api.github.com/orgs/$SOURCE_ORG/repos?per_page=100&page=$page")
 
   # Get repo names from current page
@@ -39,7 +38,7 @@ done
 # Loop through each repository and transfer it
 for repo in "${repos[@]}"; do
   echo "Transferring repository: $repo"
-  curl -s -X POST -H "Authorization: token $GITHUB_TOKEN_DEST_ORG" \
+  curl -s -X POST -H "Authorization: token $GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/$SOURCE_ORG/$repo/transfer" \
     -d "{\"new_owner\":\"$DEST_ORG\"}"
